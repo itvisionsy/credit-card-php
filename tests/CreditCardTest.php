92ff279f88d5975e7d6bd1e378855c4dc9efe2a1
Muhannad Shelleh <muhannad.shelleh@gmail.com>
2017-06-10 18:05:37 +0400

    Init

---
+++ tests/CreditCardTest.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Muhannad Shelleh <muhannad.shelleh@live.com>
+ * Date: 6/10/17
+ * Time: 5:36 PM
+ */
+
+use ItvisionSy\CreditCard\CreditCard;
+
+class CreditCardTest extends PHPUnit_Framework_TestCase
+{
+
+    public function testCardType()
+    {
+    }
+
+    public function testMakeStatic()
+    {
+    }
+
+    public function testSettersAndGetters()
+    {
+    }
+
+    public function testChecks()
+    {
+    }
+
+}
